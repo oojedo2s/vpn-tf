089b605e0b7ae846016c3a29ab2133f9b9c6ffc8
oojedo2s <ojedokunseyi@gmail.com>
2020-12-15 02:08:13 +0100

    added new changes

--- ovpn.sh
+++ ovpn.sh
@@ -6,6 +6,10 @@ bg2="\e[1;31m"
 ed="\e[0m"
 user=$(whoami)
 curl="sudo curl -s"
+sed="sudo sed -i -e"
+sh="sudo sh -c"
+pub_int=$(ip route list default | awk -F ' ' '{print $5}')
+pub_int_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 vars="/home/$user/easy-rsa/vars"
 
 $e "$bg Enter CA server ip address:  $ed"
@@ -21,11 +25,12 @@ else
     ln -s /usr/share/easy-rsa/* /home/$user/easy-rsa/
 
     sudo chown $user /home/$user/easy-rsa
+    # Make file executable
     chmod 700 /home/$user/easy-rsa
 
     touch $vars
-    echo -e "set_var EASYRSA_ALGO \"ec\"" > $vars
-    echo -e "set_var EASYRSA_DIGEST \"sha512\"" > $vars
+    $e "set_var EASYRSA_ALGO \"ec\"" > $vars
+    $e "set_var EASYRSA_DIGEST \"sha512\"" > $vars
 
     #Create directory to store client certificates and files
     mkdir -p /home/$user/client-configs/keys
@@ -97,11 +102,102 @@ client_cert() {
         sudo cp /tmp/$client_name'.crt' /home/$user/client-configs/keys/
     
         sudo chown $user.$user /home/$user/client-configs/keys/*
-        
+
     elif [[ $option == revoke ]]; then
         servcertrev="$curl -F "file=@$client_name" https://$ip/revokecert $key"
     fi
 }
 client_cert
 
+# Configure OpenVPN
+sudo cp /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz /etc/openvpn/server/
+sudo gunzip /etc/openvpn/server/server.conf.gz
 
+# Edit Server config file
+# Change HMAC
+$sed 's/tls-auth ta.key 0/tls-crypt ta.key/g' /etc/openvpn/server/server.conf
+#Change encryption cipher
+$sed 's/cipher AES-256-CBC/cipher AES-256-GCM/g' /etc/openvpn/server/server.conf
+# Set HMAC message digest algorithm as SHA 256
+$sh "echo 'auth SHA256' >> /etc/openvpn/server/server.conf"
+# Remove Diffie Hellman since ellipttic curve is used instead
+$sed 's/dh dh2048.pem/dh none/g' /etc/openvpn/server/server.conf
+#Enable OpenVPN to run without privileges
+$sed 's/;user nobody/user nobody/g' /etc/openvpn/server/server.conf
+$sed 's/;group nogroup/group nogroup/g' /etc/openvpn/server/server.conf
+#Push DNS to redirect all traffic through VPN
+$sed 's/;push "redirect-gateway def1 bypass-dhcp"/push "redirect-gateway def1 bypass-dhcp"/g' /etc/openvpn/server/server.conf
+#Use free OpenDNS resolvers
+$sed 's/;push "dhcp-option DNS 208.67.222.222"/push "dhcp-option DNS 208.67.222.222"/g' /etc/openvpn/server/server.conf
+$sed 's/;push "dhcp-option DNS 208.67.220.220"/push "dhcp-option DNS 208.67.220.220"/g' /etc/openvpn/server/server.conf
+#Adjust default IP forwarding 
+$sh "echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf"
+
+# Enable firewall
+sudo ufw enable
+
+# Edit firewall to add masquerading
+cat <<EOF >/etc/ufw/before.rules
+# START OPENVPN NAT RULES
+*nat
+:POSTROUTING ACCEPT [0:0]
+# Allow traffic from OpenVPN client to public interface
+-A POSTROUTING -s 10.8.0.0/8 -o $pub_int -j MASQUERADE
+COMMIT
+# END OPENVPN NAT RULES
+EOF
+
+# Enable UFW to forward packets by default
+$sed 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/g' /etc/openvpn/server/server.conf
+
+# Allow traffic to OpenVPN and re-enable firewall
+sudo ufw allow 1194/udp
+sudo ufw disable
+sudo ufw enable
+
+# Enable OpenVPN to start up at boot
+sudo systemctl -f enable openvpn-server@server.service
+
+# Start OpenVPN and check status
+sudo systemctl start openvpn-server@server.service
+sudo systemctl status openvpn-server@server.service
+
+# Create folder to store client config files & set up base conf
+mkdir -p /home/$user/client-configs/files
+cp /usr/share/doc/openvpn/examples/sample-config-files/client.conf /home/$user/client-configs/base.conf
+# Set remote Public IP of OpenVPN server
+$sed "s/remote my-server-1 1194/remote $pub_int_ip 1194/g" /home/$user/client-configs/base.conf
+# Remove privileges (non-Windows only)
+$sed 's/;user nobody/user nobody/g' /home/$user/client-configs/base.conf
+$sed 's/;group nogroup/group nogroup/g' /home/$user/client-configs/base.conf
+# Enable CA, cert and key 
+$sed 's/;ca ca.crt/ca ca.crt/g' /home/$user/client-configs/base.conf
+$sed 's/;cert client.crt/cert client.crt/g' /home/$user/client-configs/base.conf
+$sed 's/;key client.key/key client.key/g' /home/$user/client-configs/base.conf
+# Enable tls-auth
+$sed 's/;tls-auth ta.key 1/tls-auth ta.key 1/g' /home/$user/client-configs/base.conf
+# Add cipher and auth
+$sed "s/cipher AES-256-CBC/cipher AES-256-GCM/g" > /home/$user/client-configs/base.conf
+$e "auth SHA256" > /home/$user/client-configs/base.conf
+# Set key-direction to 1
+$e "key-direction 1" > /home/$user/client-configs/base.conf
+
+
+# To generate client .ovpn files as quickly as possible
+# usage: 1. make script executable
+#        2. run script followed by common name of client e.g ./ovpn.sh clienta
+KEY_DIR=/home/$user/client-configs/keys
+OUTPUT_DIR=/home/$user/client-configs/files
+BASE_CONFIG=/home/$user/client-configs/base.conf
+
+cat ${BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    ${KEY_DIR}/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    ${KEY_DIR}/${1}.crt \
+    <(echo -e '</cert>\n<key>') \
+    ${KEY_DIR}/${1}.key \
+    <(echo -e '</key>\n<tls-crypt>') \
+    ${KEY_DIR}/ta.key \
+    <(echo -e '</tls-crypt>') \
+    > ${OUTPUT_DIR}/${1}.ovpn
